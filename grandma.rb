a09372c84da12f0c52a1d95aca93b0daf8d50a62
Halie Hall <halierae17@outlook.com>
2021-04-11 21:31:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == "Hi Nana, how are you?" || "Hi!"
+    elsif phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
     else phrase == "WHAT?"
       return "NO, NOT SINCE 1938!"
